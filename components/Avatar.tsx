505ac2bf15d132e634134b0df1e8de5aad87ad3a
SparkOW511 <timm.gamertv@gmail.com>
2024-03-31 21:28:39 +0200

    second commit

---
+++ components/Avatar.tsx
@@ -0,0 +1,19 @@
+interface AvatarProps {
+    userId: string;
+    isLarge?: boolean;
+    hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+    userId,
+    isLarge,
+    hasBorder
+}) => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default Avatar;
