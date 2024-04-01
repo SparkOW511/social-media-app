a1287a386c94524a6c9c116d7cf5eacf948909e8
SparkOW511 <timm.gamertv@gmail.com>
2024-04-01 18:32:00 +0200

    Finished User Profile

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,7 @@ import useUser from "@/hooks/useUser";
 import { useMemo } from 'react';
 import Button from '../Button';
 import { BiCalendar } from 'react-icons/bi';
+import useEditModal from '@/hooks/useEditModal';
 
 interface UserBioProps {
     userId: string;
@@ -13,6 +14,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     const { data: currentUser } = useCurrentUser();
     const { data: fetchedUser } = useUser(userId);
+
+    const  editModal = useEditModal();
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -26,7 +29,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         <div className="border-b-[1px] border-neutral-800 pb-4">
             <div className="flex justify-end p-2">
                 {currentUser?.id === userId ? (
-                    <Button secondary label="Edit" onClick={() => {}} />
+                    <Button secondary label="Edit" onClick={editModal.onOpen} />
                 ) : (
                     <Button
                         onClick={() => {}}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import EditModal from "@/components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
