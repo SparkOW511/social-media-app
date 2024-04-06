810a47b2804b02707853f1e84ced633612b39cd2
SparkOW511 <timm.gamertv@gmail.com>
2024-04-06 17:13:39 +0200

    added comments

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,6 +7,7 @@ import { useCallback, useState } from "react";
 import toast from "react-hot-toast";
 import Button from "./Button";
 import Avatar from "./Avatar";
+import usePost from "@/hooks/usePost";
 
 interface FormProps {
     placeholder: string;
@@ -24,6 +25,7 @@ const Form: React.FC<FormProps> = ({
 
     const { data: currentUser } = useCurrentUser();
     const { mutate: mutatePosts } = usePosts();
+    const { mutate: mutatePost } = usePost(postId as string);
 
     const [body, setBody] = useState('');
     const [isLoading, setIsLoading] = useState(false);
@@ -32,18 +34,21 @@ const Form: React.FC<FormProps> = ({
         try {
             setIsLoading(true);
 
-            await axios.post('/api/posts', { body });
-            
+            const url = isComment ? `/api/comments?postId=${postId}` : '/api/posts';
+
+            await axios.post(url, { body });
+ 
             toast.success('Post Created');
 
             setBody('');
             mutatePosts();
+            mutatePost();
         } catch (error) {
             toast.error('Something went wrong');
         } finally {
             setIsLoading(false);
         }
-    }, [body, mutatePosts])
+    }, [body, mutatePosts, isComment, postId, mutatePost])
 
     return ( 
         <div className="border-b-[1px] border-neutral-800 px-5 py-2">
@@ -106,6 +111,6 @@ const Form: React.FC<FormProps> = ({
             )}
         </div>
      );
-}
+};
  
 export default Form;
