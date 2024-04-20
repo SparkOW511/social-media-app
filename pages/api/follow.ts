07bec7bb399ef5898c83698b90b7cdb3a0164921
SparkOW511 <timm.gamertv@gmail.com>
2024-04-20 10:37:15 +0200

    fixed following function

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -19,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const user = await prisma.user.findUnique({
       where: {
-        id: userId
+        id: currentUser.id
       }
     });
 
